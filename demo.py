208c07797d1a5600aa57f284ebeff55c0c618e8d
alanyoung7 <alanyoung7@gmail.com>
2019-12-11 17:15:58 -0500

    Demo of voice interaction

---
+++ demo.py
@@ -0,0 +1,17 @@
+
+import os
+import time
+
+os.system("espeak 'Alexa.'")
+os.system("espeak '.'")
+os.system("espeak 'Play, adventure----time, game'")
+time.sleep(23.5)
+os.system("espeak 'Win-doe.'")
+
+time.sleep(5)
+        
+            
+        
+        
+        
+    
